77a852d5756b06265949e54a243625048d0dab00
Vitoriano <vitorianoernandes2012@gmail.com>
2022-11-03 23:06:19 -0300

    Testando a verificação do stock

--- src/modules/product-adm/facade/product-adm.facade.spec.ts
+++ src/modules/product-adm/facade/product-adm.facade.spec.ts
@@ -49,5 +49,27 @@ describe('Product-adm Facade Test', () => {
     expect(productDb.description).toEqual(input.description);
   });
 
+  it("Should check  product stock", async() => {
+
+    const productFacade = ProductAdmFacadeFactory.create();
+
+    const input = {
+      id: "1",
+      name: "Product 1",
+      description: "Description",
+      purchasePrice: 10,
+      stock: 10
+    }
+
+    await productFacade.addProduct(input);
+
+    const result = await productFacade.checkStock({ productId: input.id});
+
+    expect(result.productId).toEqual(input.id);
+    expect(result.stock).toEqual(input.stock);
+   
+  });
+
+
 
 });
--- src/modules/product-adm/factory/facade.factory.ts
+++ src/modules/product-adm/factory/facade.factory.ts
@@ -1,15 +1,17 @@
 import ProducAdmFacade from "../facade/product-adm-facade";
 import ProductRepository from "../repository/product.repository";
 import AddProductUseCase from "../usecase/add-product/add-product.usecase";
+import CheckStockUseCase from "../usecase/check-stock/check-stock.usecase";
 
 export default class ProductAdmFacadeFactory {
 
   static create() {
     const productRepository = new ProductRepository();
     const addProductUseCase = new AddProductUseCase(productRepository);
+    const checkStockUseCase = new CheckStockUseCase(productRepository);
     const productFacade = new ProducAdmFacade({
       addUseCase: addProductUseCase,
-      stockUseCase: undefined
+      stockUseCase: checkStockUseCase
     });
 
     return productFacade;
