aad894e6d4ed7f3815359a1d492d3695fb883993
Vitoriano <vitorianoernandes2012@gmail.com>
2022-11-07 23:41:35 -0300

    Finalizando o findproductusecase

--- src/modules/store-catalog/repository/product.repository.ts
+++ src/modules/store-catalog/repository/product.repository.ts
@@ -20,8 +20,22 @@ export default class ProductRepository implements ProductGateway {
 
   }
 
-  find(id: string): Promise<Product> {
-    throw new Error("Method not implemented.");
+  async find(id: string): Promise<Product> {
+
+    const product = await ProductModel.findOne({
+      where: { id},
+    });
+
+    if(!product) {
+      throw new Error(`Product with id ${id} not find`);
+    }
+
+    return new Product({
+      id: new Id(product.id),
+      name: product.name,
+      description: product.name,
+      salesPrice: product.salesPrice
+    })
   }
 
 }
